7b30b109852a7b20aa5a4b21dcea567c930f3e6a
m.dornseif@hudora.de <m.dornseif@hudora.de@2a7d6d32-97f8-0310-8fd3-c67d3a265405>
2009-12-27 00:58:27 +0000

    handle missing dimensions gracefully
    git-svn-id: https://cybernetics.hudora.biz/intern/svn/code/projects/huImages/trunk@7442 2a7d6d32-97f8-0310-8fd3-c67d3a265405

--- setup.py
+++ setup.py
@@ -6,18 +6,19 @@ from setuptools import setup, find_packages
 setup(name='huImages',
       maintainer='Maximillian Dornseif',
       maintainer_email='md@hudora.de',
-      version='2.0p1',
+      version='2.0p3',
       url='http://github.com/hudora/huImages/',
       description='Image storage & scaling in CouchDB.',
       long_description=long_description,
       classifiers=['License :: OSI Approved :: BSD License',
                    'Intended Audience :: Developers',
                    'Programming Language :: Python'],
-      package_data={"huimages": ["imagebrowser/templates/imagebrowser/*.html"]},
+      # CARVE! setuptools seems buggy in regard of sdist & package_data
+      package_data={"huImages": ["huimages/imagebrowser/templates/imagebrowser/*.html"]},
       zip_safe=False,
       packages=find_packages(),
       include_package_data=True,
       install_requires=['couchdb', 'huTools', 'boto'], # PIL/Imaging has serious issues with easy_install
-      dependency_links = ['http://cybernetics.hudora.biz/dist/', 'http://cybernetics.hudora.biz/nonpublic/eggs/',
+      dependency_links = ['http://cybernetics.hudora.biz/nonpublic/eggs/',
                           'http://www.pythonware.com/products/pil/'],
 )
